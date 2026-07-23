25d7674f56ed7eb647b22bd53208ec6c4af6006d
RIFFAT KHATTAK <anasktk246@gmail.com>
2026-07-23 21:05:21 +0000

    Complete BE-01 Fastapi assignmebt

---
+++ Backend/BE-01/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="BE-01 First API",
+    description="My first FastAPI backend application for FlyRank AI Internship.",
+    version="1.0"
+)
+
+
+@app.get("/")
+def root():
+    return {
+        "message": "Hello! Welcome to my first FastAPI backend.",
+        "author": "Riffat"
+    }
+
+
+@app.get("/status")
+def status():
+    return {
+        "status": "success",
+        "message": "Backend server is running successfully."
+    }
